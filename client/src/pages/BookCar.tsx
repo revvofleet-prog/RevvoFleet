66f70a80b551e2ef84d6da8e60e27fc9bf24e793
Uday Kumar <udaykumar5706@gmail.com>
2026-01-20 20:04:24 +0530

    Update site

--- client/src/pages/BookCar.tsx
+++ client/src/pages/BookCar.tsx
@@ -192,8 +192,8 @@ export default function BookCar() {
 
     emailjs
       .send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        "service_r2oks7i",
+        "template_dj1ov1k",
         emailData,
         "Lx5DU97sEEROihwB7"
       )
